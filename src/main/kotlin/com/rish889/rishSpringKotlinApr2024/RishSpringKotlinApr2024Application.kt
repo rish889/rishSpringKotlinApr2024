88f931e650f7aa0b7dd1973d2720f21709dd46f2
Rishabh Garcha <rish889@gmail.com>
2024-04-11 12:04:05 +0530

    Message Repository

--- src/main/kotlin/com/rish889/rishSpringKotlinApr2024/RishSpringKotlinApr2024Application.kt
+++ src/main/kotlin/com/rish889/rishSpringKotlinApr2024/RishSpringKotlinApr2024Application.kt
@@ -2,8 +2,12 @@ package com.rish889.rishSpringKotlinApr2024
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @SpringBootApplication
 class RishSpringKotlinApr2024Application
@@ -13,13 +17,35 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageController {
-    @GetMapping("/") //http://localhost:8080
-    fun index() = listOf(
-        Message("1", "Hello!"),
-        Message("2", "Bonjour!"),
-        Message("3", "Privet!"),
-    )
+class MessageController(val service: MessageService) {
+    @GetMapping("/")
+    fun index(): List<Message> = service.findMessages()
+
+    @GetMapping("/{id}")
+    fun index(@PathVariable id: String): List<Message> =
+        service.findMessageById(id)
+
+    @PostMapping("/")
+    fun post(@RequestBody message: Message) {
+        service.save(message)
+    }
 }
 
-data class Message(val id: String?, val text: String)
+interface MessageRepository : CrudRepository<Message, String>
+
+@Table("MESSAGES")
+data class Message(@Id var id: String?, val text: String)
+
+@Service
+class MessageService(val db: MessageRepository) {
+    fun findMessages(): List<Message> = db.findAll().toList()
+
+    fun findMessageById(id: String): List<Message> = db.findById(id).toList()
+
+    fun save(message: Message) {
+        db.save(message)
+    }
+
+    fun <T : Any> Optional<out T>.toList(): List<T> =
+        if (isPresent) listOf(get()) else emptyList()
+}
