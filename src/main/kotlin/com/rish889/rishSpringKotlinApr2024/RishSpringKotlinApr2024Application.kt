1e93c715be8f9ac2f536cbf67729ec25fdac6f83
Rishabh Garcha <rish889@gmail.com>
2024-04-11 09:00:19 +0530

    Initial Commit

---
+++ src/main/kotlin/com/rish889/rishSpringKotlinApr2024/RishSpringKotlinApr2024Application.kt
@@ -0,0 +1,11 @@
+package com.rish889.rishSpringKotlinApr2024
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RishSpringKotlinApr2024Application
+
+fun main(args: Array<String>) {
+	runApplication<RishSpringKotlinApr2024Application>(*args)
+}
---
+++ src/test/kotlin/com/rish889/rishSpringKotlinApr2024/RishSpringKotlinApr2024ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rish889.rishSpringKotlinApr2024
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RishSpringKotlinApr2024ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
