861f254e8656b4d243f945f3bbf63c0725fd523d
Rishabh Garcha <rish889@gmail.com>
2024-04-11 10:29:12 +0530

    Message Controller

--- src/main/kotlin/com/rish889/rishSpringKotlinApr2024/RishSpringKotlinApr2024Application.kt
+++ src/main/kotlin/com/rish889/rishSpringKotlinApr2024/RishSpringKotlinApr2024Application.kt
@@ -2,10 +2,19 @@ package com.rish889.rishSpringKotlinApr2024
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class RishSpringKotlinApr2024Application
 
 fun main(args: Array<String>) {
     runApplication<RishSpringKotlinApr2024Application>(*args)
+}
+
+@RestController
+class MessageController {
+    @GetMapping("/") //http://localhost:8080?name=John
+    fun index(@RequestParam("name") name: String) = "Hello, $name!"
 }
